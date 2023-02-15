f1380cb15a6d4fffef2c7c8fde5b1bb7e320f104
root <s-saito@chic.ocn.ne.jp>
2023-02-15 13:17:44 +0000

    add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kaggle_lecr
+
+## バックグラウンドでの動作
+nohup ./do_nbconvert.sh ipynb/finetuning-mpnet-tripletloss-10man.ipynb &
+
+## 動いているかの確認
+ps aux | grep python
--- do_nbconvert.sh
+++ do_nbconvert.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(time jupyter nbconvert --to notebook --execute --inplace $1) > output/$(date +"%Y%m%d%I%M%S").txt 2>&1
+pip install runnb
+(time runnb --allow-not-trusted $1) > output/$(TZ=JST-9 date +"%Y%m%d%I%M%S").txt
