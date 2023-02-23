e53a6c7288fd6ec0955446972d04b4b4e6d6ee9c
root <s-saito@chic.ocn.ne.jp>
2023-02-23 06:47:22 +0000

    add

--- do_notebook_by_cli.sh
+++ do_notebook_by_cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install runnb
-(time runnb --allow-not-trusted --to=$1 $1) > output/$(TZ=JST-9 date +"%Y%m%d%I%M%S").txt
+(time runnb --allow-not-trusted --to=$1 $1) > output/$(TZ=JST-9 date +"%Y%m%d%H%M%S").txt 2>&1
